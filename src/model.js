e96ac3bd1b77365056df0de3d79325b42cf58998
volodymyr bondariev <vladmikhbond@hotmail.com>
2021-12-06 16:25:07 +0200

    revers permutation

--- src/model.js
+++ src/model.js
@@ -59,21 +59,19 @@ function decode(variants)
     const stages = [
 
         ["a", "ax = 0; ay=0;"],
-        ["ab", "ax = 0; ay = 0; bx = 0; by = 0;"],
+        ["ab",   "ax = 0; ay = 0; bx = 0; by = 0;"],
         ["a-ab", "ax = 0; ay = 0; bx = 0; by = 0;"],
-        ["abc", "ax = 0; ay = 0; bx = 0; by = 0; cx = 0; cy = 0;"],
-        ["a-abc", "ax = 0; ay = 0; bx = 0; by = 0; cx = 0; cy = 0;"],
-        ["ab-abc", "ax = 0; ay = 0; bx = 0; by = 0; cx = 0; cy = 0;"],
+        ["abc",      "ax = 0; ay = 0; bx = 0; by = 0; cx = 0; cy = 0;"],
+        ["a-abc",    "ax = 0; ay = 0; bx = 0; by = 0; cx = 0; cy = 0;"],
+        ["ab-abc",   "ax = 0; ay = 0; bx = 0; by = 0; cx = 0; cy = 0;"],
         ["a-ab-abc", "ax = 0; ay = 0; bx = 0; by = 0; cx = 0; cy = 0;"],
-
-        ["ab-ac", "ax = 0; ay = 0; bx = -ar + br; by = 0; cx = ar - cr; cy = 0;"],
-        ["a-ab-ac", "ax = 0; ay = 0; bx = -ar + br; by = 0; cx = ar - cr; cy = 0;"],
-
-        // ["ab-abc-ac", [x, y], [x, y], [x, y]],
-        // ["a-ab-abc-ac", [x, y], [x, y], [x, y]],
-        // ["a-b", [x, y], [x, y], [x, y]],
-        // ["a-ab-b", [x, y], [x, y], [x, y]],
-        // ["a-abc-b", [x, y], [x, y], [x, y]],
+        ["ab-ac",     "ax = 0; ay = 0; bx = -ar + br; by = 0; cx = ar - cr; cy = 0;"],
+        ["a-ab-ac",   "ax = 0; ay = 0; bx = -ar + br; by = 0; cx = ar - cr; cy = 0;"], 
+        ["ab-abc-ac", "ax = 0; ay = 0; bx = -ar + br; by = 0; cx = ar - cr; cy = 0;"], 
+        ["a-ab-abc-ac",  "ax = 0; ay = 0; bx = -ar + br; by = 0; cx = ar - cr; cy = 0;"], 
+        ["a-b", "ax = -ar; ay = 0; bx = br + 10; by = 0;"],
+        ["a-ab-b",  "ax = -ar/2; ay = 0; bx = br/2; by = 0;"],
+        ["a-abc-b", "ax = 0; ay = 0; bx = ar -(2*cr - br); by = 0; cx = ar-cr; cy = 0;"], // 12345, 3456, 345
         // ["a-ab-abc-b", [x, y], [x, y], [x, y]],
         // ["ac-b", [x, y], [x, y], [x, y]],
         // ["a-ac-b", [x, y], [x, y], [x, y]],
@@ -101,24 +99,6 @@ function decode(variants)
         // ["a-ab-abc-ac-b-bc-c", [x, y], [x, y], [x, y]],
         
 
-        //-----------------------------------------
-        // ["a-b-c", [x - R, y-R], [x + R, y-R], [x, y+R]],
-        // ["a-ac-b-c", [x - r/2, y-R], [x, y + R], [x + r/2, y-R]],
-        // ["a-ac-b", [x - r/2, y-R], [x, y + R], [x - r/2, y-R]],
-        // ["a-ac-b-bc-c", [x - R, y], [x+R, y], [x, y]],
-        // ["a-ac-b-bc",   [x - R, y], [x+r, y], [x, y]],
-
-        // ["a-ab-ac", [x, y], [x-m, y], [x+r, y]],
-        // ["ab-ac",   [x, y], [x-R + m, y], [x+R-r, y]],
-        // ["a-ab-abc-ac", [x, y], [x-r, y], [x+r, y]],
-        // ["a-ab-abc", [x,y], [x,y], [x,y]],
-        // ["a-ab-abc-ac-c", [x,y], [x,y], [x + R - r,y]],
-        // ["a-ab-ac-c", [x,y], [x,y], [x + R, y]],
-        
-        // ["a-ab-abc-ac-b-bc-c", [x - m, y], [x + r, y], [x, y]],
-        // ["a-abc-b-c",  [x - R/2, y], [x + m/2, y], [x, y]],
-        // ["a-ab-abc-b",  [x - m, y], [x + r, y], [x, y]],
-        //-----------------------------------------
     ];
 
 
@@ -141,22 +121,24 @@ function decode(variants)
 function doStage(permut, stage) 
 {
     // permut -> [0,2,1]
-    let permutIndices = permut.split('').map(x => x == 'a' ? 0 : x == 'b' ? 1 : 2 );
+    //let permutIndices = permut.split('').map(x => x == 'a' ? 0 : x == 'b' ? 1 : 2 );
 
     let params = "a, b, c, x, y";
     let body = "ax = bx = cx = x; ay = by = cy = y; ar = a.r; br = b.r; cr = c.r;"
     body += "\n" + stage[1].replace(/=/g, '+=');
-    body += "a.x=ax;a.y=ay;a.r=ar;  b.x=bx;b.y=by;b.r=br;  c.x=cx;c.y=cy;c.r=cr;";
+    body += "a.x=ax; a.y=ay; a.r=ar;   b.x=bx; b.y=by; b.r=br;   c.x=cx; c.y=cy; c.r=cr;";
     const func = new Function(params, body);
 
     let x = canvas.width / 2, y = canvas.height / 2;
+    // обратная перестановка!
     switch (permut) {
-        case "abc": func(setA, setB, setC, x, y); break;
-        case "acb": func(setA, setC, setB, x, y); break;
-        case "bac": func(setB, setA, setC, x, y); break;
-        case "bca": func(setB, setC, setA, x, y); break;
-        case "cab": func(setC, setA, setB, x, y); break;
-        case "cba": func(setC, setB, setA, x, y); break;        
+        case "abc": func(setA, setB, setC, x, y); break;   
+        // abc->acb ::: abc<-acb ::: a<-a, b<-c; c<-b  :::  ACB
+        case "acb": func(setA, setC, setB, x, y); break; 
+        case "bac": func(setB, setA, setC, x, y); break;  
+        case "bca": func(setC, setA, setB, x, y); break;  
+        case "cab": func(setB, setC, setA, x, y); break;  
+        case "cba": func(setC, setB, setA, x, y); break;  
     }
 }
 
